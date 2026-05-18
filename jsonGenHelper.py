f7157523d1375bc535e1389de943adf530da8b8e
sidoschn <dominik.schneidereit@fau.de>
2026-05-18 23:00:08 +0200

    updated string curret and voltage addresses to correct values (down shifted by 2)

--- jsonGenHelper.py
+++ jsonGenHelper.py
@@ -3,8 +3,8 @@ import json
 outDict = {}
 StartReg = 142
 for i in range(1,17,1):
-    outDict["V_String"+str(i)] = {"register":4,"address":StartReg+(i*2) ,"length":1,"type":"int", "bWriteable":False, "valueMultiplier":0.1,"unit":" V", "bMakeSensor":True}
-    outDict["Curr_Sting"+str(i)] = {"register":4,"address":StartReg+1+(i*2) ,"length":1,"type":"int", "bWriteable":False, "valueMultiplier":0.1,"unit":" A", "bMakeSensor":True}
+    outDict["V_String"+str(i)] = {"register":4,"address":StartReg+((i-1)*2) ,"length":1,"type":"int", "bWriteable":False, "valueMultiplier":0.1,"unit":" V", "bMakeSensor":True}
+    outDict["Curr_Sting"+str(i)] = {"register":4,"address":StartReg+1+((i-1)*2) ,"length":1,"type":"int", "bWriteable":False, "valueMultiplier":0.1,"unit":" A", "bMakeSensor":True}
 
 
 with open ("helper.json", "w") as f:
